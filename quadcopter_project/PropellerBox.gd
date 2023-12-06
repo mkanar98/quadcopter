9eebb1d27cd839e0f6aa91ef5bc7de1a889ef529
mkanar98 <mkanar98@gmail.com>
2023-12-06 09:56:01 +0300

    added propeller box and torque impulse

---
+++ quadcopter_project/PropellerBox.gd
@@ -0,0 +1,26 @@
+extends RigidBody
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+var f0 = 1
+var propellor_angular_velocity = 0 # current
+var target_propellor_angular_velocity = f0 # target
+var angular_velocity_interpolation_rate = 1.0 # how fast we change to target
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+#func _physics_process(delta):
+	
+#	var force = Vector3(0,f0,0) # y is up
+#	var offset = Vector3(0,0,0) # for now just apply force to center - but later we can make this work for each propellor position
+#	add_force(force, offset)
+#	set_angular_velocity(Vector3(0,f0,0))
+#	apply_torque_impulse(Vector3(0,f0,0))
--- quadcopter_project/RigidBody.gd
+++ quadcopter_project/RigidBody.gd
@@ -14,38 +14,24 @@ var f0 = 10
 var propellor_angular_velocity = 0 # current
 var target_propellor_angular_velocity = f0 # target
 var angular_velocity_interpolation_rate = 1.0 # how fast we change to target
+var new_angular_velocity = 50
 
-#func set_angular_velocity(new_angular_velocity):
-#	target_propellor_angular_velocity = new_angular_velocity
+func set_angular_velocity(new_angular_velocity):
+	target_propellor_angular_velocity = new_angular_velocity
 
 #var ang_v = set_angular_velocity(Vector3(0,f0,0))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
-<<<<<<< HEAD
-#pass
-func _physics_process(delta):
-	var force = Vector3(0,10,0) # y is up
-	var offset = Vector3(0,1.25,0) # for now just apply force to center - but later we can make this work for each propellor position
-	get_angular_velocity()*delta
-	add_force(force, offset)
-	
-var propellor_angular_velocity = 0 # current
-var target_propellor_angular_velocity = 10 # target
-var angular_velocity_interpolation_rate = 1.0 # how fast we change to target
-
-func set_angular_velocity(new_angular_velocity):
-	target_propellor_angular_velocity = new_angular_velocity
-=======
 #	pass
 func _physics_process(delta):
 	
 	var force = Vector3(0,f0,0) # y is up
 	var offset = Vector3(0,0,0) # for now just apply force to center - but later we can make this work for each propellor position
-	add_force(force, offset)
-	set_angular_velocity(Vector3(0,f0,0))
+	
+	#add_force(ang, offset)
+	var ti = apply_torque_impulse(Vector3(0,f0,0))
+	#add_torque(Vector3(0,f0,0))
+	#set_angular_velocity(-new_angular_velocity)
 
-
->>>>>>> e232548e4d1345e545e26dc905dd63cb99b369d6
-
-
+	
