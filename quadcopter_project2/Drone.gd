1aa52dafd03edf0c890ac02d5505d60451732145
mkanar <mkanar98@gmail.com>
2023-12-14 16:53:21 +0300

    drone class

--- quadcopter_project2/Drone.gd
+++ quadcopter_project2/Drone.gd
@@ -1,3 +1,4 @@
+class_name Drone
 extends RigidBody
 
 onready var propellor_collision = $PropellorCollision
@@ -7,17 +8,25 @@ onready var chassis_visual = $ChassisVisual
 
 var propellor_spin_rate = 0 # this is the actual rate the propellor is spinning
 
-var target_velocity = Vector3(0,2,0)
-#func move(sx, sy, sz):
-#	control = Vector3(sx, sy, sz) 
+
+var target_velocity = Vector3(0, 0, 0)
+
+func move(sx, sy, sz):
+	target_velocity =  Vector3(sx, sy, sz) 
 
 var speed = Vector3(0,0,0)
 	
 func _ready():	
+	move(0,1,0)
 	linear_velocity.y = 0
 	linear_velocity.x = 0
+	
+var K = 10.0
+var A = 0.01
+var B = 0.2 # this will become some factor - in reality it is based on moment of inertia of propellor
 
 func _physics_process(delta):
+
 	set_friction(0)
 
 
@@ -28,10 +37,10 @@ func _physics_process(delta):
 	var delta_velocity = target_velocity - linear_velocity           # this is the change we need in velocity
 	
 	# now add up lift here based on propellor speed
-	var K = 100.0
+	
 	var up_force = get_weight() + delta_velocity.y*K
 	var side_force = delta_velocity.x*K
-	var A = 0.01
+
 	var old_propellor_spin_rate = propellor_spin_rate
 	propellor_spin_rate = up_force/A # slowly speed up
 
@@ -40,7 +49,7 @@ func _physics_process(delta):
 	#speed += delta_velocity*K*delta
 	# calculate torque - when we change our propellor speed there is acounter torque
 	var delta_spin_rate = propellor_spin_rate - old_propellor_spin_rate
-	var B = 0.2 # this will become some factor - in reality it is based on moment of inertia of propellor
+
 	var torque = -delta_spin_rate * B
 	add_torque(Vector3(0,torque,0)) # this makes box move in opposite direction
 	
@@ -48,7 +57,7 @@ func _physics_process(delta):
 	
 
 	add_force(Vector3(side_force,up_force,0), Vector3(0,0,0))
-	print(delta, "   ", target_velocity.x-linear_velocity.x, "   ", target_velocity.y -linear_velocity.y,"   ", side_force, "   ", up_force-get_weight(), "   ", speed)
+	#print(target_velocity.y -linear_velocity.y,"   ", up_force-get_weight(), "   ", speed)
 	#if get_linear_velocity() < Vector3(0,1.1,0) and get_linear_velocity() > Vector3(0,9.9,0):
 	#if propellor_spin_rate > 1126:
 	#	add_force(Vector3(0,-get_weight()-up_force,0), Vector3(0,0,0))
